0b4d80843e76f7bc5c0785e749a5ab337d792f92
Eduardo Aguilar <eaguilar92105@yahoo.com>
2024-02-05 16:14:00 -0800

    added scroll view and tested with hard code to make sure it works

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,31 +1,50 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import React from 'react';
+import {Dimensions} from 'react-native';
+
+//Get device Width
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
+const thirds = screenWidth / 3;
 
 const Profile = () => {
   return (
     <View style={styles.body}>
-      <View style={styles.banner} />
-      <Text style={styles.txt}>Grand Hand Slam</Text>
-      <View style={styles.pic} />
-      <Text style={styles.user}>ReigningEddie</Text>
-      <View style={styles.flex}>
-        <View style={styles.grid}>
-          <Text style={styles.num}>150</Text>
-          <Text style={styles.item}>points</Text>
+      <ScrollView
+        contentContainerStyle={styles.contentContainer}
+        scrollEnabled={true}
+        nestedScrollEnabled={true}>
+        <View style={styles.banner} />
+        <Text style={styles.txt}>Grand Hand Slam</Text>
+        <View style={styles.pic} />
+        <Text style={styles.user}>ReigningEddie</Text>
+        <View style={styles.flex}>
+          <View style={styles.grid}>
+            <Text style={styles.num}>150</Text>
+            <Text style={styles.item}>points</Text>
+          </View>
+          <View style={styles.grid}>
+            <Text style={styles.num}>361</Text>
+            <Text style={styles.item}>followers</Text>
+          </View>
+          <View style={styles.grid}>
+            <Text style={styles.num}>253</Text>
+            <Text style={styles.item}>following</Text>
+          </View>
         </View>
-        <View style={styles.grid}>
-          <Text style={styles.num}>361</Text>
-          <Text style={styles.item}>followers</Text>
+        <Text style={styles.dez}>Description (add max characters)</Text>
+        <View style={styles.portrait}>
+        <Text style={styles.vids} />
+          <Text style={styles.vids} />
+          <Text style={styles.vids} />
+          <Text style={styles.vids} />
+          <Text style={styles.vids} />
+          <Text style={styles.vids} />
+          <Text style={styles.vids} />
+          <Text style={styles.vids} />
         </View>
-        <View style={styles.grid}>
-          <Text style={styles.num}>253</Text>
-          <Text style={styles.item}>following</Text>
-        </View>
-      </View>
-      <Text style={styles.dez}>Description (add max characters)</Text>
-      <View style={styles.portrait}>
-        <Text style={styles.vids}>Hello</Text>
-      </View>
+      </ScrollView>
     </View>
   );
 };
@@ -34,9 +53,14 @@ export default Profile;
 
 const styles = StyleSheet.create({
   body: {
-    flex: 1,
     backgroundColor: '#FFFFFF',
     alignItems: 'center',
+    height: screenHeight,
+  },
+  contentContainer: {
+    flexGrow: 1,
+    alignItems: 'center',
+    width: screenWidth,
   },
   banner: {
     backgroundColor: '#284B63',
@@ -86,10 +110,20 @@ const styles = StyleSheet.create({
   },
   portrait: {
     backgroundColor: '#284B63',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     width: '100%',
     height: '100%',
   },
   vids: {
-    color: '#284B63',
+    backgroundColor: 'grey',
+    borderWidth: 1,
+    borderColor: '#284B63',
+    width: thirds,
+    height: 200,
+  },
+  size: {
+    height: 10,
+    backgroundColor: 'black',
   },
 });
