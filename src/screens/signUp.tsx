c5e7c13d7049ed96a56895f71261a7fc6e174f62
Eduardo Aguilar <eaguilar92105@yahoo.com>
2024-05-19 17:24:24 -0700

    .

--- src/screens/signUp.tsx
+++ src/screens/signUp.tsx
@@ -53,9 +53,9 @@ export default function SignUp({navigation}: NavProps) {
       }
       await createUser.user.updateProfile({displayName: data.username});
       await firestore().collection('users').doc(createUser.user.uid).set({
+        mobileNumber: data.mobileNumber,
         firstName: data.firstName,
         lastName: data.lastName,
-        mobileNumber: data.mobileNumber,
       });
       await createUser.user.sendEmailVerification();
     } catch (err: any) {
