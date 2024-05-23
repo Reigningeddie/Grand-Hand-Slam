9aaa713819ed8deb36e69007e171275cbdc67a31
Eduardo Aguilar <eaguilar92105@yahoo.com>
2024-05-23 16:35:33 -0700

    fixed fetdata.ts bug

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -1,9 +1,7 @@
 import firestore from '@react-native-firebase/firestore';
 
-const fetchData = async () => {
+export const fetchData = async () => {
   try{
     const collect = await firestore().collection('users').get();
-    const data = [];
     console.log(collect);
   }
-}
