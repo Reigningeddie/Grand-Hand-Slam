62b18fbe7e8ca8fca7116493cbb656a3ee9e318e
Eduardo Aguilar <eaguilar92105@yahoo.com>
2025-03-06 19:26:35 -0800

    added a quick search box

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
 
 const Search = () => {
   return (
     <View>
-      <Text>Search</Text>
+      <TextInput
+        style={styles.input}
+        placeholder={'Search'}/>
     </View>
   )
 }
 
 export default Search
+
+const styles = StyleSheet.create({
+  input: {
+    color: '#1B1B1B',
+    fontSize: 15,
+    borderWidth: 2,
+    borderRadius: 5,
+    height: 40,
+    margin: 15,
+    paddingLeft: 15,
+  }
+})
