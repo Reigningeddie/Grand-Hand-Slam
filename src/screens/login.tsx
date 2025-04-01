56712cfc656c76c397e7a9fa20260db8b87ed924
Eduardo Aguilar <eaguilar92105@yahoo.com>
2025-04-01 16:10:21 -0700

    made adjustments to first agreement to the rules

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -112,7 +112,7 @@ export default function Login({navigation}: NavProps): React.JSX.Element {
             Hello, and welome to Grand Hand Slam
           </Text>
           <Text style={styles.modalTxt}>
-          Before we begin, we must have deliberate aknowledgement of the rules for this game as well as ask for your full cooperation before begining. By agreeing and signing up, you will FOREVER BE IN THIS GAME. If forever is too long of a commitment then feel free to leave the app now. There will be retributions if a player sees you breaking rules. You will be excommunicated if you are constinuously reported or deemed by a commissioner an unworthy or dishonest player, and you will be forever disgraced and put in to the HALL OF SHAME for all to see. Be aware this is a gentle-man/woman's drinking game. You will conduct yourself as a gentleman or gentlewoman by following the rules. Please drink responsibly and continue at your own risk.
+          Before we begin, we must have deliberate aknowledgement of the rules for this game as well as ask for your full cooperation. By agreeing and signing up, you will FOREVER BE IN THIS GAME. If forever is too long of a commitment then feel free to leave the app now. There will be retributions if a player sees you breaking rules. You will be excommunicated if you are constinuously reported or deemed by a commissioner an unworthy or dishonest player, and you will be forever disgraced and put in to the HALL OF SHAME for all to see. Be aware this is a gentle-man/woman's drinking game. You will conduct yourself as a gentleman or gentlewoman by following the rules. Please drink responsibly and continue at your own risk.
             </Text>
         </View>
       </Modal>
