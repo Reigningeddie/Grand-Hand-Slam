ddc691897e3ec5511fae624a8af429c727fc99a5
Eduardo Aguilar <eaguilar92105@yahoo.com>
2025-06-05 17:42:24 -0700

    added supabase search query function

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -1,15 +1,72 @@
-import { View, TextInput, StyleSheet } from 'react-native'
-import React from 'react'
-import {useAuth} from '../database/authContext';
-
-
+import {View, TextInput, StyleSheet, Text, FlatList} from 'react-native'
+import React, {useState, useEffect} from 'react'
+import {supabase} from '../database/supabase';
 
 const Search = () => {
+  const [query, setQuery] = useState('');
+  const [result, setResult] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (query) {
+      const timer = setTimeout(() => {
+        setLoading(true);
+        searchDb(query);
+      }, 300);
+      return () => clearTimeout(timer);
+    } else {
+      setResult([]);
+    }
+  }, [query]);
+
+  const searchDb = async (query:string) => {
+    if (!query.trim()) {
+      setResult([]);
+      return;
+    }
+
+    try {
+      const {data, error} = await supabase
+        .from('profile')
+        .select('*')
+        .ilike('userName', `%${query}%`);
+
+      if (error) throw error;
+
+      setResult(data);
+    } catch (error:any) {
+      console.error('Search error:', error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  console.log(result)
+
   return (
     <View>
       <TextInput
         style={styles.input}
-        placeholder={'Search'}/>
+        placeholder={'Search by name'}
+        value={query}
+        onChangeText={setQuery} />
+
+        {loading ? (
+        <Text>Loading...</Text>
+      ) : result.length > 0 ? (
+        <FlatList
+          data={result}
+          keyExtractor={(item) => item.id.toString()} 
+          renderItem={({ item }) => (
+            <View>
+              {/* <Text>{`${item.firstName} ${item.lastName}`}</Text> */}
+              <Text style={{ color: 'gray' }}>{item.userName}</Text>
+            </View>
+          )}
+        />
+      ) : (
+        <Text>No results found</Text>
+      )}
     </View>
   )
 }
